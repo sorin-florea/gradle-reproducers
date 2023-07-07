0e96194fe777f8e9eebb7e3d5d43764ec17af1fb
Sorin Florea <sflorea@gradle.com>
2023-07-07 14:52:08 +0200

    Add reproducer for repository with same URL

---
+++ dependencies-various-source-repo/src/test/java/example/ExampleTest.java
@@ -0,0 +1,14 @@
+
+            package example;
+            
+            public class ExampleTest {
+                
+@org.junit.Test
+public void aTest() throws Exception {
+    Thread.sleep(10);
+    System.out.println("some standard output");
+    System.err.println("some error output");
+}
+
+            }
+        
